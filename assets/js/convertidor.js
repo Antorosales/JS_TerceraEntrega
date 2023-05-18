93f97aa7397a91876178f73f3bd4063596b6b5a6
Antorosales <129124164+Antorosales@users.noreply.github.com>
2023-05-18 09:47:09 -0400

    HTML Y CSS

---
+++ assets/js/convertidor.js
@@ -0,0 +1,4 @@
+const lista = document.querySelector("#lista");
+const valor = document.getElementById("Valor");
+const buttonConvertir = document.getElementById("convertir");
+const buttonMostrar = document.getElementById("mostrarLista");
